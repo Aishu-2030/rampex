ee7c6dab3009f88bc363296b639685361d54730c
Aishu-2030 <aishuaishwariya233@gmail.com>
2025-05-14 15:27:09 +0530

    Add files via upload

---
+++ day3/operators.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+ int main()
+ {
+    ch op;
+    int a=5,b=9;
+    printf("enter the operators:");
+    scanf("%c",&op);
+    {
+        case '+':
+        printf("result is +%d\n",a+b);
+        break;
+        case '-':
+        printf("result is -%d\n",a-b);
+        break;
+        case'*':
+        printf("result is *%d\n",a*b);
+        break;
+        case'/':
+        printf("esult is /%d\n".a/b);
+        break;
+    }
+    result 0;
+    
+    }
+ }
